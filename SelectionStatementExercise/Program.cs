906433cd24adb7728b4395edea30b732bfedfd2f
samtech36 <samuel.hossain@outlook.com>
2024-08-13 13:06:24 -0400

    My Number Guess Game Program

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            NumberGame();
+        }
+
+        public static void NumberGame()
+
+        {
+            Random r = new Random();
+            int favoriteNumber = r.Next(1, 20);
+            Console.WriteLine("Hello. Please Pick a number from 1 to 1000: ");
+            int userInput;
+
+            do
+            {
+                
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favoriteNumber)
+                {
+                    Console.WriteLine($"the number {userInput} is too low");
+                }
+
+                else if (userInput > favoriteNumber)
+                {
+                    Console.WriteLine($"the number {userInput} is too high");
+                }
+
+                else
+                {
+                    Console.WriteLine("Congrats. You guessed the number");
+                }
+
+            } while (userInput != favoriteNumber);
+
+
         }
     }
 }
